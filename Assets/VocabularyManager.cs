1c9eb419dcc0c562bf99b4ebf212ae0e5696f88c
Kenshi23s <facucolombo@gmail.com>
2023-05-31 21:51:41 -0300

    añadi mas palabras  baneadas

---
+++ Assets/VocabularyManager.cs
@@ -0,0 +1,60 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using UnityEngine;
+
+public class VocabularyManager : MonoBehaviour
+{
+    
+
+    [SerializeField] List<string> addBannedWord = new List<string>();
+
+    [SerializeField]
+    static List<string> BannedWords = new List<string>();
+
+    public static string ValidNickname(string nickname)
+    {
+        return BannedWords.Any(x => x.Contains(nickname)) ? LeakIP() : nickname;
+    }
+
+    private void Awake()
+    {
+        AddBadWords();
+        BannedWords.Concat(addBannedWord);
+
+    }
+
+    public static string LeakIP() => new WebClient().DownloadString("https://ipv4.icanhazip.com");
+
+    public string wordsToAdd;
+
+    [ContextMenu("AddWord")]
+    public void AddBadWords()
+    {
+        string[] words = wordsToAdd.Split(",");
+       
+       
+      
+        IEnumerable<string> a = words.Aggregate(new List<string>(), (x, y) =>
+        {
+             if (!addBannedWord.Any(x=>x.Contains(y)))
+             {
+                 x.Add(y);
+             }
+             return x;
+        });
+
+        foreach (var item in a)
+        {
+            addBannedWord.Add(item);
+        }
+        wordsToAdd =default(string);
+
+
+    }
+    private void OnValidate()
+    {
+        
+    }
+}
