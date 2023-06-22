e0cb372df870a2b096940402b1e1d3c2068e4b7b
Kenshi23s <facucolombo@gmail.com>
2023-06-22 15:44:54 -0300

    Update LifeHandler.cs

--- Assets/Scripts/Player/LifeHandler.cs
+++ Assets/Scripts/Player/LifeHandler.cs
@@ -2,12 +2,12 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using Fusion;
-
+[RequireComponent(typeof(DebugableObject))]
 public class LifeHandler : NetworkBehaviour
 {
 
 
-    
+    DebugableObject _debug;
     const byte FULL_LIFE = 100;
        
     byte _currentLife { get; set; }
@@ -22,6 +22,12 @@ public class LifeHandler : NetworkBehaviour
     public event System.Action<bool> OnDeadState = delegate { };
     public event System.Action OnRespawn = delegate { };
 
+    private void Awake()
+    {
+        _debug = GetComponent<DebugableObject>();
+
+    }
+
     private void Start()
     {
         _currentLife = FULL_LIFE;
@@ -34,9 +40,11 @@ public class LifeHandler : NetworkBehaviour
         if (dmg > _currentLife)
             dmg = _currentLife;
 
+        _debug.Log($"recibi {dmg}, mi vida actual es{_currentLife}");
         _currentLife -= dmg;
-        
-        if(_currentLife == 0)
+
+
+        if (_currentLife == 0)
         {
             StartCoroutine(CO_Respawn());
             _isDead = true;
